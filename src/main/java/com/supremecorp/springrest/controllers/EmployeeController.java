ec23e118e3f3953731df5e1ca492d6b8c5820bb0
Suprememajor <ndipnobertetta@gmail.com>
2022-09-08 13:46:23 +0100

    added EmployeeController

---
+++ src/main/java/com/supremecorp/springrest/controllers/EmployeeController.java
@@ -0,0 +1,52 @@
+package com.supremecorp.springrest.controllers;
+
+import com.supremecorp.springrest.domains.Employee;
+import com.supremecorp.springrest.repositories.EmployeeRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class EmployeeController {
+    private final EmployeeRepository repository;
+
+    @GetMapping("/employees")
+    List<Employee> all() {
+        return repository.findAll();
+    }
+
+    @PostMapping("/employees")
+    Employee newEmployee(@RequestBody Employee newEmployee) {
+        return repository.save(newEmployee);
+    }
+
+    @GetMapping("/employees/{id}")
+    Employee one(@PathVariable Integer id) {
+
+        return repository.findById(id)
+                .orElseThrow(() -> new EmployeeNotFoundException(id));
+    }
+
+    @PutMapping("/employees/{id}")
+    Employee replaceEmployee(@RequestBody Employee newEmployee, @PathVariable Integer id) {
+
+        return repository.findById(id)
+                .map(employee -> {
+                    employee.setName(newEmployee.getName());
+                    employee.setRole(newEmployee.getRole());
+                    return repository.save(employee);
+                })
+                .orElseGet(() -> {
+                    newEmployee.setId(id);
+                    return repository.save(newEmployee);
+                });
+    }
+
+    @DeleteMapping("/employees/{id}")
+    void deleteEmployee(@PathVariable Integer id) {
+        repository.deleteById(id);
+    }
+
+}
